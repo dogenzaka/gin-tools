ce450872182ef96afb406f2b73794eb61c62600e
Osamu TONOMORI <osamingo@users.noreply.github.com>
2014-10-29 23:06:40 +0900

    Update middleware.go

--- validation/middleware.go
+++ validation/middleware.go
@@ -1,13 +1,17 @@
 package validation
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // ValidatePathParam ... validate for path parameter
 func ValidatePathParam(name string, vs ...Validator) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		for _, v := range vs {
 			if !v.Validate(c.Params.ByName(name)) {
-				c.Abort()
+				c.AbortWithStatus(http.StatusBadRequest)
 				return
 			}
 		}
@@ -19,7 +23,7 @@ func ValidateRequestParam(name string, vs ...Validator) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		for _, v := range vs {
 			if !v.Validate(c.Request.FormValue(name)) {
-				c.Abort()
+				c.AbortWithStatus(http.StatusBadRequest)
 				return
 			}
 		}
