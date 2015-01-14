bda5296bc8de8a17fe29eb49c3ac4fe3567aeec4
Osamu TONOMORI <osamingo@gmail.com>
2015-01-14 19:47:20 +0900

    Refactor middleware.go

--- validation/middleware.go
+++ validation/middleware.go
@@ -1,6 +1,8 @@
 package validation
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -45,14 +47,42 @@ func ValidateRequestParam(name string, vs ...Validator) gin.HandlerFunc {
 // ValidateRequestHeader validate for request header
 func ValidateRequestHeader(name string, vs ...Validator) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		p := c.Request.Header.Get(name)
+		h := c.Request.Header.Get(name)
 		for _, v := range vs {
-			if !v.Validate(p) {
+			if !v.Validate(h) {
 				c.Abort()
 				c.JSON(http.StatusBadRequest, map[string]interface{}{
 					"code":    "bad_params",
 					"message": "bad params",
-					"params":  map[string]string{name: p},
+					"params":  map[string]string{name: h},
+				})
+				return
+			}
+		}
+	}
+}
+
+// ValidateRequestBody validate for request body
+func ValidateRequestBody(vs ...Validator) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		b, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			c.Abort()
+			c.JSON(http.StatusBadRequest, map[string]interface{}{
+				"code":    "bad_params",
+				"message": "bad params",
+				"params":  map[string]string{"body": "failed read body"},
+			})
+			return
+		}
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+		for _, v := range vs {
+			if !v.Validate(string(b)) {
+				c.Abort()
+				c.JSON(http.StatusBadRequest, map[string]interface{}{
+					"code":    "bad_params",
+					"message": "bad params",
+					"params":  map[string]string{"body": string(b)},
 				})
 				return
 			}
